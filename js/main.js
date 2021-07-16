10c5e76227cc4fd2a4606bca7ad2409f2b640835
casci <cascio713@gmail.com>
2021-07-16 16:36:15 +0200

    aggiunte lingue e aggoistato overflow dei filtri all'apertura di un dropdown

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var nAuthors = 0;
 var count1 = 0;
 var count2 = 0;
 
-var addCols = function (){
+var addCols = function () {
 
     var myCol = $('<div class="form-group col-md-6"><label>Language:</label><select class="form-select" aria-label="Default select example"><option selected>Choose</option><option value="1">One</option><option value="2">Two</option><option value="3">Three</option></select></div>');
 
@@ -11,83 +11,83 @@ var addCols = function (){
 
 };
 
-$(document).ready(function(){
+$(document).ready(function () {
     addCols();
     return false;
 });
 
-$(document).ready(function() {
-  $("#btnGen").click(function() {
-      addCols();
-      return false;
-  });
+$(document).ready(function () {
+    $("#btnGen").click(function () {
+        addCols();
+        return false;
+    });
 });
 
-$(document).ready(function() {
- $("#btnFilter").click(function() {
+$(document).ready(function () {
+    $("#btnFilter").click(function () {
 
-    var myDiv = document.getElementById('filter-panel');
+        var myDiv = document.getElementById('filter-panel');
 
-    if (myDiv.style.display == 'block') {
-      myDiv.style.display = 'none';
-    } else {
-      myDiv.style.display = 'block';
-    }
- });
-});
-
-$(window).resize(function() {
-  if ($(window).width() > 576) {
-     var myDiv = document.getElementById('filter-panel');
-     myDiv.style.display = 'block';
-  }
-  if ($(window).width() <= 576) {
-       var myDiv = document.getElementById('filter-panel');
-       myDiv.style.display = 'none';
-    }
-});
-
-$(document).ready(function() {
-var checkList = document.getElementById('languages-filter');
-checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
-  if (checkList.classList.contains('visible'))
-    checkList.classList.remove('visible');
-  else
-    checkList.classList.add('visible');
-}
-});
-
-$(document).ready(function() {
- $("#btnHome").click(function() {
-
-    var myDiv = document.getElementById('home');
-    var myDiv2 = document.getElementById('users');
-
-     if (myDiv.style.display != 'block') {
-          myDiv.style.display = 'block';
-          myDiv2.style.display = 'none';
-          this.classList.add("active");
-          document.getElementById("btnAut").classList.remove("active");
+        if (myDiv.style.display == 'block') {
+            myDiv.style.display = 'none';
+        } else {
+            myDiv.style.display = 'block';
         }
- });
+    });
 });
 
-$(document).ready(function() {
- $("#btnAut").click(function() {
-
-    var myDiv = document.getElementById('users');
-    var myDiv2 = document.getElementById('home');
-
-    if (myDiv.style.display != 'block') {
-      myDiv.style.display = 'block';
-      myDiv2.style.display = 'none';
-      this.classList.add("active");
-      document.getElementById("btnHome").classList.remove("active");
+$(window).resize(function () {
+    if ($(window).width() > 576) {
+        var myDiv = document.getElementById('filter-panel');
+        myDiv.style.display = 'block';
     }
- });
+    if ($(window).width() <= 576) {
+        var myDiv = document.getElementById('filter-panel');
+        myDiv.style.display = 'none';
+    }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
+    var checkList = document.getElementById('languages-filter');
+    checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+        if (checkList.classList.contains('visible'))
+            checkList.classList.remove('visible');
+        else
+            checkList.classList.add('visible');
+    }
+});
+
+$(document).ready(function () {
+    $("#btnHome").click(function () {
+
+        var myDiv = document.getElementById('home');
+        var myDiv2 = document.getElementById('users');
+
+        if (myDiv.style.display != 'block') {
+            myDiv.style.display = 'block';
+            myDiv2.style.display = 'none';
+            this.classList.add("active");
+            document.getElementById("btnAut").classList.remove("active");
+        }
+    });
+});
+
+$(document).ready(function () {
+    $("#btnAut").click(function () {
+
+        var myDiv = document.getElementById('users');
+        var myDiv2 = document.getElementById('home');
+
+        if (myDiv.style.display != 'block') {
+            myDiv.style.display = 'block';
+            myDiv2.style.display = 'none';
+            this.classList.add("active");
+            document.getElementById("btnHome").classList.remove("active");
+        }
+    });
+});
+
+$(document).ready(function () {
 
     var count1 = $("#accordionFlushExample .accordion-item").length
     document.getElementById("counterArticles").textContent = count1;
@@ -97,7 +97,7 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     count1 = $("#accordionFlushExample .accordion-item").length;
     document.getElementById("counter").textContent = count1;
@@ -109,53 +109,51 @@ $(document).ready(function() {
 
 });
 
-var checkVisibility = function (){
+var checkVisibility = function () {
 
-    $("#accordionFlushExample .accordion-item").slice(nArticles-6, nArticles).hide();
-    $("#accordionFlushAutExample .accordion-item").slice(nAuthors-6, nAuthors).hide();
+    $("#accordionFlushExample .accordion-item").slice(nArticles - 6, nArticles).hide();
+    $("#accordionFlushAutExample .accordion-item").slice(nAuthors - 6, nAuthors).hide();
 
-    $("#accordionFlushExample .accordion-item").slice(nArticles+6, nArticles+12).hide();
-    $("#accordionFlushAutExample .accordion-item").slice(nAuthors+6, nAuthors+12).hide();
+    $("#accordionFlushExample .accordion-item").slice(nArticles + 6, nArticles + 12).hide();
+    $("#accordionFlushAutExample .accordion-item").slice(nAuthors + 6, nAuthors + 12).hide();
 
-    $("#accordionFlushExample .accordion-item").slice(nArticles, nArticles+6).show();
-    $("#accordionFlushAutExample .accordion-item").slice(nAuthors, nAuthors+6).show();
-
+    $("#accordionFlushExample .accordion-item").slice(nArticles, nArticles + 6).show();
+    $("#accordionFlushAutExample .accordion-item").slice(nAuthors, nAuthors + 6).show();
 
 
 };
 
-$(document).ready(function() {
-   $("#btnPrevResArt").click(function() {
+$(document).ready(function () {
+    $("#btnPrevResArt").click(function () {
 
-      nArticles -= 6;
-      if(nArticles < 0){
-         nArticles = 0;
-      }
-      checkVisibility();
-   });
+        nArticles -= 6;
+        if (nArticles < 0) {
+            nArticles = 0;
+        }
+        checkVisibility();
+    });
 
-   $("#btnNextResArt").click(function() {
-       if(count1 > 6 && nArticles < count1-6){
-         nArticles += 6;
-       }
-       checkVisibility();
-   });
+    $("#btnNextResArt").click(function () {
+        if (count1 > 6 && nArticles < count1 - 6) {
+            nArticles += 6;
+        }
+        checkVisibility();
+    });
 
-   $("#btnPrevResAut").click(function() {
+    $("#btnPrevResAut").click(function () {
 
-      nAuthors -= 6;
-      if(nAuthors < 0){
-         nAuthors = 0;
-      }
-      checkVisibility();
-   });
-   $("#btnNextResAut").click(function() {
-       if(count2 > 6 && nAuthors < count2-6){
-         nAuthors += 6;
-       }
-       checkVisibility();
-   });
-
+        nAuthors -= 6;
+        if (nAuthors < 0) {
+            nAuthors = 0;
+        }
+        checkVisibility();
+    });
+    $("#btnNextResAut").click(function () {
+        if (count2 > 6 && nAuthors < count2 - 6) {
+            nAuthors += 6;
+        }
+        checkVisibility();
+    });
 
 
 });
