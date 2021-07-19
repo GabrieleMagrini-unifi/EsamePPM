e18d9c1406135290c47900e10e3ff473311ebab5
manu <cascio713@gmail.com>
2021-07-19 14:40:57 +0200

    aggiunto offset -1 al numero di risultati per pagina

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+
+def execute_query(query: str):
+    return []
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/resources", methods=['GET', 'POST'])
+def resources_search_result():
+    # dizioniario roba get
+    sparql_query_elements = request.values
+    query = ""
+    query_results = execute_query(query)
+    return render_template("resources_search.html", query_results)
